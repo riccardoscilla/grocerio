2c3bc630716eff02d4bbba4f4488efca7cbca3b5
riccardoscilla <riccardo.scilla@gmail.com>
2024-12-18 19:05:54 +0100

    cors origin

--- src/main/java/com/grocerio/security/SecurityConfig.java
+++ src/main/java/com/grocerio/security/SecurityConfig.java
@@ -47,8 +47,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "https://riccardoscilla.github.io"
+                "http://localhost:4200"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
